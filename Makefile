5a3c0e825237a64b8c2bf50726c51f0583ef6f8a
Lodovico Giaretta <Lodovico Giaretta>
2016-04-27 21:57:21 +0200

    Added W3C XML Test Suite; added testing driver

--- Makefile
+++ Makefile
@@ -3,12 +3,14 @@
 help:
 	@echo Available commands:
 	@echo - benchmark: builds and benchmarks the library
-	@echo - clean: deletes all executables and libraries
-	@echo - test: builds and executes unittests
+	@echo - clean: deletes all binaries
+	@echo - help: shows this text
+	@echo - test: builds and executes compliance tests
+	@echo - unittest: builds and executes unittests
 
 .PHONY: benchmark
 benchmark:
-	dub run -c benchmark
+	dub run -c benchmark -b release
 
 .PHONY: clean
 clean:
@@ -16,7 +18,11 @@ clean:
 	$(RM) __test__library__
 	$(RM) std-experimental-xml
 	$(RM) lib*.a
-	
+
 .PHONY: test
 test:
-	dub test
+	dub run -c test -b debug
+	
+.PHONY: unittest
+unittest:
+	dub test -b unittest
---
+++ source/test.d
@@ -0,0 +1,111 @@
+
+module test;
+
+import std.experimental.xml.lexers;
+import std.experimental.xml.parser;
+import std.experimental.xml.cursor;
+
+import std.file: readText;
+import std.path;
+import std.stdio;
+
+auto indexes = 
+[
+    "tests/eduni/xml-1.1/xml11.xml",
+];
+
+/++
++ Most tests are currently not working for two reasons:
++ 1) We don't have any validation, so we accept all files that seem well formed;
++ 2) std.file.readText rejects files with encoding iso-8859.
++/
+void main()
+{
+    auto cursor = XMLCursor!(Parser!(SliceLexer!string))();
+    
+    int failed;
+    int total;
+    foreach (i, index; indexes)
+    {
+        cursor.setSource(readText(index));
+        cursor.enter();
+        
+        if (cursor.getName() != "TESTCASES")
+            continue;
+            
+        write(i);
+        foreach (att; cursor.getAttributes())
+            if (att[1] == "PROFILE")
+                write(" -- ", att[2]);
+        writeln(" -- ", index);
+        
+        cursor.enter();
+        L: do
+        {
+            bool positive = true;
+            string file;
+            foreach (att; cursor.getAttributes())
+                if (att[1] == "ENTITIES" && att[2] != "none")
+                    continue L;
+                else if (att[1] == "TYPE" && att[2] == "not-wf")
+                    positive = false;
+                else if(att[1] == "URI")
+                    file = att[2];
+            
+            writeln("\t", file);
+            
+            total++;
+            bool passed = true;
+            Throwable error;
+            try
+            {
+                parseFile(dirName(index) ~ dirSeparator ~ file);
+            }
+            catch (Throwable err)
+            {
+                passed = false;
+                error = err;
+            }
+            if(passed != positive)
+            {
+                failed++;
+                writeln("  == ERROR ==  ");
+                if (!passed)
+                {
+                    writeln(error.info);
+                    writeln(error);
+                }
+                writeln("File ", file, " should", positive?"":" not", " be accepted!");
+                cursor.enter();
+                writeln(cursor.getText());
+                cursor.exit();
+            }
+        }
+        while (cursor.next());
+        cursor.exit();
+    }
+    
+    writeln("  == RESULTS ==  ");
+    writeln(failed, " tests failed out of ", total, " total.");
+}
+
+void parseFile(string filename)
+{
+    void inspectOneLevel(T)(T cursor)
+    {
+        do
+        {
+            if (cursor.hasChildren())
+            {
+                cursor.enter();
+                inspectOneLevel(cursor);
+                cursor.exit();
+            }
+        }
+        while (cursor.next());
+    }
+
+    auto cursor = XMLCursor!(Parser!(SliceLexer!string))();
+    cursor.setSource(readText(filename));
+    inspectOneLevel(cursor);
+}
