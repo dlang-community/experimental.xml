fe9ffac6e518e16739b66cfb574a850b01d6b205
Lodovico Giaretta <Lodovico Giaretta>
2016-07-23 23:22:23 +0200

    travis_after_success.sh more bugfixes

--- travis_after_success.sh
+++ travis_after_success.sh
@@ -1,11 +1,12 @@
 if [ ${PRIMARY} = "true" ] && [ ${TRAVIS_BRANCH} = "master" ] && [ ${TRAVIS_PULL_REQUEST} = "false" ]
 then
     make docs
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lodo1995/experimental.xml.git gh-pages > /dev/null
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lodo1995/experimental.xml.git gh-pages
     cd gh-pages
     git rm -rf .
-    cp -R ../docs/* ./*
+    cp -r ../docs/* .
     git add .
+    git commit -m "Documentation updated by Travis CI (build $TRAVIS_BUILD_NUMBER)"
     git push https://${GH_TOKEN}@github.com/lodo1995/experimental.xml.git gh-pages
     
     bash <(curl -s https://codecov.io/bash)
