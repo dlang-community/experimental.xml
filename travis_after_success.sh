851f2960224de2ca74f8b9805801f7ba1149050d
Lodovico Giaretta <Lodovico Giaretta>
2016-07-23 23:44:33 +0200

    More travis_after_success bugfixes

--- travis_after_success.sh
+++ travis_after_success.sh
@@ -1,6 +1,8 @@
 if [ ${PRIMARY} = "true" ] && [ ${TRAVIS_BRANCH} = "master" ] && [ ${TRAVIS_PULL_REQUEST} = "false" ]
 then
     make docs
+    git config --global user.email "lodo1995@users.noreply.github.com"
+    git config --global user.name "travis-ci"
     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lodo1995/experimental.xml.git gh-pages
     cd gh-pages
     git rm -rf .
