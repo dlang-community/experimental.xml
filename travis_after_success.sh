a13cf3e243d44674e630196b91fc59583e5b9dee
Lodovico Giaretta <Lodovico Giaretta>
2016-07-23 23:12:40 +0200

    travis_after_success.sh bugfix

--- travis_after_success.sh
+++ travis_after_success.sh
@@ -1,4 +1,4 @@
-if [ ${PRIMARY} = "true" ] && [ ${TRAVIS_BRANCH} = "master" ] && [ ${TRAVIS_PULL_REQUEST = "false"} ]
+if [ ${PRIMARY} = "true" ] && [ ${TRAVIS_BRANCH} = "master" ] && [ ${TRAVIS_PULL_REQUEST} = "false" ]
 then
     make docs
     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lodo1995/experimental.xml.git gh-pages > /dev/null
